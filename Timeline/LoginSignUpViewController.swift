63c428fc76ca8cc0e95c669cd4ccda14de1f2edc
Diego-F-Aguirre <diego3884@gmail.com>
2016-02-23 15:01:07 -0700

    View Hierarchy completed and User Model created

---
+++ Timeline/LoginSignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginSignUpViewController.swift
+//  Timeline
+//
+//  Created by Diego Aguirre on 2/23/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
